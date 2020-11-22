d454bb784e8b5f92efdd6afab0729b74f9a6c31b
fnreality <59921084+fnreality@users.noreply.github.com>
2020-11-21 20:58:14 -0500

    Should work

--- test.clj
+++ test.clj
@@ -2,22 +2,28 @@
   []
   (agent (with-meta {} {
                         :snowball true
-                        :sent-keys #{}}))
+                        :sent-keys #{}})))
 
 (defn base!
   [sb base-key base-val]
-  (send sb
-    #(assoc % base-key base-val)))
+  (send sb #(-> %
+    (assoc base-key base-val)
+    (vary-meta update-in :sent-keys
+      #(conj % result)))))
 
 (defn step!
   [sb result needed-keys func]
-  (when-not (@sb result)
-    (let [
-          uses (map @sb needed-keys)]
-      (when (every? identity uses)
-        (send sb
-          #(assoc % result
-            (apply func uses)))))))
+  (let [
+        uses (map @sb needed-keys)]
+    (when (and
+        (every? identity ((juxt :snowball
+            (comp not result :sent-keys)
+            (constantly uses))
+          (meta @sb))))
+      (send sb #(-> %
+        (assoc result (apply func uses))
+        (vary-meta update-in :sent-keys
+          #(conj % result)))))))
 
 ;;TEST
 
